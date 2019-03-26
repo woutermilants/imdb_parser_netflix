d78f626bc1c9a6d27bfab989587b2e5e47ae4ba0
wmilants <wouter.milants@esaturnus.com>
2019-03-26 16:37:42 +0100

    try to login to netflix fails

--- imdb/imdb_parser.py
+++ imdb/imdb_parser.py
@@ -1,71 +1,64 @@
 from html.parser import HTMLParser
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
+import requests
 
-class ImdbHtmlParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.recording = 0
-        self.data = []
-    def handle_starttag(self, tag, attributes):
-        #print("in start")
-        if tag != 'div':
-            return
-        if self.recording:
-            self.recording += 1
-            return
-        for name, value in attributes:
-            if name == 'class' and value == 'lister-item mode-simple':
-                break
-        else:
-            return
-        self.recording = 1
+class ImdbScraper:
 
-    def handle_endtag(self, tag):
-        #print("in end")
-        if tag == 'div' and self.recording:
-            self.recording -= 1
-
-    def handle_data(self, data):
-        if self.recording :
-            if '\\n' in data.strip() or not data.strip():
-                return
-            print("abc" + data.strip()+"cde")
-            self.data.append(data)
-
-    def print(self):
-        print(*self.data)
-
-html_page = html_page = urllib2.urlopen("https://www.imdb.com/search/title?groups=top_250&view=simple&sort=user_rating,desc&ref_=adv_prv")
-
-#Feeding the content
-
-parser = ImdbHtmlParser()
-#parser.feed(str(html_page.read()))
-#parser.print()
-
-
-class Bs:
+    movie_rating = {}
 
     def tryWithBs(self):
-        html_page = html_page = urllib2.urlopen("https://www.imdb.com/search/title?groups=top_250&view=simple&sort=user_rating,desc&ref_=adv_prv")
-
+        html_page = urllib2.urlopen("https://www.imdb.com/search/title?groups=top_250&view=simple&sort=user_rating,desc&ref_=adv_prv")
         contents = html_page.read()
-
         soup = BeautifulSoup(contents, 'lxml')
 
-  #      print(soup.select("div"))
+        listerItems = soup.findAll("div", {"class": "lister-item mode-simple"})
+        for listerItem in listerItems:
+            coltitle = listerItem.findAll("div", {"class": "col-title"})
+            movie = ''
+            for col in coltitle:
+                children = col.findChildren("a")
+                for child in children:
+                    movie = child.string
+                    print(child.string)
+            ratings = listerItem.findAll("div", {"class": "col-imdb-rating"})
+            for rating in ratings:
+                print(rating.findChildren("strong")[0].string.strip())
+                rating = rating.findChildren("strong")[0].string.strip()
+                self.movie_rating[movie] =  rating
 
-        coltitle = soup.findAll("div", {"class": "col-title"})
-        for col in coltitle:
-            children = col.findChildren("a")
-            for child in children:
-                print(child.string)
+    def findMovieOnNetflix(self, movie):
+        print("netflix " + movie)
+        netflixSearchPage = "https://www.netflix.com/search?q=" + movie.replace("'", "").replace(" ", "%20")
+        print(netflixSearchPage)
+        html_page = urllib2.urlopen(netflixSearchPage)
+        contents = html_page.read()
+        soup = BeautifulSoup(contents, 'lxml')
+
+#        suggestionsExist = soup.find("div", {"class": "suggestions"})
+        suggestionsExist = soup.find("div")
+        print(suggestionsExist)
+
+    def testLogin(self, movie):
+        url = movie
+        values = {'userLoginId': 'woutmilants@gmail.com',
+          'password': 'netflixWouter2019'}
+
+        r = requests.post(url, data=values)
+        print(r.content)
 
 
 
-test = Bs()
+test = ImdbScraper()
 test.tryWithBs()
+
+
+#test.findMovieOnNetflix("schindlers list")
+test.testLogin("https://www.netflix.com/be-en/Login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fsearch%3Fq%3Dschindlers%2Blist")
+
+#print(test.movie_rating)
+#for key in test.movie_rating:
+#    test.findMovieOnNetflix(key)
 
 
 
