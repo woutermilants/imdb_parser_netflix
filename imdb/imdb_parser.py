93d9b90fc457fa05a629e8d48fc844c7cceab6b3
wmilants <wouter.milants@esaturnus.com>
2019-03-26 15:31:28 +0100

    entire list via beautiful soup

--- imdb/imdb_parser.py
+++ imdb/imdb_parser.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 import urllib.request as urllib2
+from bs4 import BeautifulSoup
 
 class ImdbHtmlParser(HTMLParser):
     def __init__(self):
@@ -7,24 +8,29 @@ class ImdbHtmlParser(HTMLParser):
         self.recording = 0
         self.data = []
     def handle_starttag(self, tag, attributes):
+        #print("in start")
         if tag != 'div':
             return
         if self.recording:
             self.recording += 1
             return
         for name, value in attributes:
-            if name == 'class' and value == 'col-title':
+            if name == 'class' and value == 'lister-item mode-simple':
                 break
         else:
             return
         self.recording = 1
 
     def handle_endtag(self, tag):
+        #print("in end")
         if tag == 'div' and self.recording:
             self.recording -= 1
 
     def handle_data(self, data):
-        if self.recording:
+        if self.recording :
+            if '\\n' in data.strip() or not data.strip():
+                return
+            print("abc" + data.strip()+"cde")
             self.data.append(data)
 
     def print(self):
@@ -35,7 +41,32 @@ html_page = html_page = urllib2.urlopen("https://www.imdb.com/search/title?group
 #Feeding the content
 
 parser = ImdbHtmlParser()
-parser.feed(str(html_page.read()))
-parser.print()
+#parser.feed(str(html_page.read()))
+#parser.print()
+
+
+class Bs:
+
+    def tryWithBs(self):
+        html_page = html_page = urllib2.urlopen("https://www.imdb.com/search/title?groups=top_250&view=simple&sort=user_rating,desc&ref_=adv_prv")
+
+        contents = html_page.read()
+
+        soup = BeautifulSoup(contents, 'lxml')
+
+  #      print(soup.select("div"))
+
+        coltitle = soup.findAll("div", {"class": "col-title"})
+        for col in coltitle:
+            children = col.findChildren("a")
+            for child in children:
+                print(child.string)
+
+
+
+test = Bs()
+test.tryWithBs()
+
+
 
 
